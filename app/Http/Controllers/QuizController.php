376e4c1d423b94e5a96cd9e91bf2035381ac74ae
Levi Farley <28295432+levifarley@users.noreply.github.com>
2022-08-02 23:16:24 -0700

    Move input validation into form request

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -4,15 +4,16 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CarQuizRequest;
 use App\Jobs\ProcessQuizSubmission;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class QuizController extends Controller
 {
     /**
      * Build and return quiz application
+     *
      * @return View
      */
     public function index(): View
@@ -24,19 +25,13 @@ class QuizController extends Controller
     /**
      * Handle quiz submission
      *
-     * @param Request $request
+     * @param CarQuizRequest $request
      * @return mixed
      */
-    public function submit(Request $request): Response
+    public function submit(CarQuizRequest $request): Response
     {
-        // TODO: Use custom form requests to handle validation for multiple quiz types
-        $validatedData = $request->validate([
-            'car' => 'required|integer',
-            'color' => 'required|integer'
-        ]);
-
         // TODO: Broadcast model event and send result back to the front-end after submission is processed
-        ProcessQuizSubmission::dispatch($validatedData)->afterResponse(); // For now just process immediately
+        ProcessQuizSubmission::dispatch($request->validated())->afterResponse(); // For now just process immediately
 
         return response('Accepted', 202);
     }
